51962cb409a1060b0758eb258f3eac0f56f8a056
Bruno Souza <61894533+BrSouza98@users.noreply.github.com>
2022-03-30 16:28:54 -0300

    Aula
    Teste

---
+++ aula-teste.py
@@ -0,0 +1,21 @@
+agente_valorant4 = 'Viper'
+agente_valorant5 = 'Kayo'
+
+#lista dos agentes
+lista_agentes_valorant = ['Yoru', 'Phoenix', 'Jett', 'Viper', 'Kayo', 'Yoru']
+
+#antes de excluir o segundo Yoru
+print(lista_agentes_valorant)
+
+#metodo remove o valor especificado entre ()
+lista_agentes_valorant.remove('Yoru')
+
+#depois de excluir o segundo Yoru
+print(lista_agentes_valorant)
+
+#antes de add a Reyna
+print(lista_agentes_valorant)
+
+#add objeto entre parenteses do append
+lista_agentes_valorant.append('Reyna')
+print(lista_agentes_valorant)
